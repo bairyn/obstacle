66355728808d0d59243a35f0428070fee505f3f9
bob <KrabbyKrap@gmail.com>
2009-02-13 18:59:17 -0700

    adjust some vote things

--- src/game/g_local.h
+++ src/game/g_local.h
@@ -1591,6 +1591,7 @@ extern  vmCvar_t  g_allowDonate;
 
 extern  vmCvar_t  g_ocReview;
 extern  vmCvar_t  g_ocOnly;
+extern  vmCvar_t  g_allowHiding;
 extern  vmCvar_t  g_allowHideVote;
 extern  vmCvar_t  g_allowUnhideVote;
 extern  vmCvar_t  g_hideTimeCallvoteMinutes;
