f39a46c54b5cbdd95e5d2b8cfdac68997e21fe58
bob <KrabbyKrap@gmail.com>
2009-02-18 21:26:36 -0700

    fix buildables and update build script so that now ui doesn't have to be recompiled on every build

--- build.sh
+++ build.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
+
+# vars
+
+build_serverdir=~/.tremulousoc/oc
+build_webdir=/var/www/oc
+build_pakname=${1}
+build_pakonly=0
+
+# make pak
+
 if [ $# -lt 1 ]; then
-	echo -ne "./build.sh <pakname>.pk3\n"
+	echo -ne "./build.sh <pakname>.pk3 (serverdir) (webdir) (pakonly)\nExample: ./build.sh data-2.0-oc-dev ~/.tremulousoc/oc /var/www\nExample: ./build.sh data-2.0-oc-dev . . yes\n(The second argument and the third agument, both '.', in the second example will be used by the script because pakonly is set)\n"
 	exit 0
+fi
+
+if [ $# -ge 2 ]; then
+	build_serverdir=${2}
+fi
+
+if [ $# -ge 3 ]; then
+	build_webdir=${3}
+fi
+
+if [ $# -ge 4 ]; then
+	if [ "$4" == "y" ] || [ "$4" == "-y" ] || [ "$4" == "yes" ] || [ "$4" == "--yes" ] || [ "$4" == "-yes" ] || [ "$4" == "1" ] || [ "$4" == "true" ]; then
+		build_pakonly=1
+	fi
 fi
 
 if ! make; then
@@ -12,35 +36,33 @@ if ! make debug; then
 	exit
 fi
 
-if [ -d "build/release-linux-x86/base/configs" ]; then
-	if ! rm -r build/release-linux-x86/base/configs; then
+if [ -d "pak_tmp" ]; then
+	if ! rm -r pak_tmp; then
 		exit
 	fi
 fi
 
-if ! cp -R ./configs build/release-linux-x86/base/configs; then
+if ! mkdir ./pak_tmp; then
 	exit
 fi
 
-if [ -d "build/release-linux-x86/base/ui" ]; then
-	if ! rm -r build/release-linux-x86/base/ui; then
-		exit
-	fi
-fi
-
-if ! cp -R ./ui build/release-linux-x86/base/ui; then
+if ! mkdir ./pak_tmp/vm; then
 	exit
 fi
 
-if ! cp build/release-linux-x86/base/vm/game.qvm ~/.tremulousoc/oc/vm/; then
+if ! cp -R ./configs ./pak_tmp/configs; then
 	exit
 fi
 
-if ! cp build/debug-linux-x86/base/gamex86.so ~/.tremulousoc/oc/; then
+if ! cp -R ./ui ./pak_tmp/ui; then
 	exit
 fi
 
-if ! cd build/release-linux-x86/base/; then
+if ! cp ./build/release-linux-x86/base/vm/cgame.qvm ./pak_tmp/vm/; then
+	exit
+fi
+
+if ! cp ./build/release-linux-x86/base/vm/ui.qvm ./pak_tmp/vm/; then
 	exit
 fi
 
@@ -50,21 +72,35 @@ if [ -e ${1}.pk3 ]; then
 	fi
 fi
 
-if ! zip -9r ../../../${1}.pk3 vm/cgame.qvm vm/ui.qvm ui configs; then
+if ! cd pak_tmp/; then
 	exit
 fi
 
-if ! cd ../../../; then
+if ! zip -9r ../${1}.pk3 vm/cgame.qvm vm/ui.qvm ui configs; then
 	exit
 fi
 
-if ! cp ${1}.pk3 /var/www/oc/; then
+if ! cd ../; then
 	exit
 fi
 
-if ! cp ${1}.pk3 ~/.tremulousoc/oc/; then
+# copy into server
+
+if ! cp build/release-linux-x86/base/vm/game.qvm ${build_serverdir}/; then
+	exit
+fi
+
+if ! cp build/debug-linux-x86/base/gamex86.so ${build_serverdir}/; then
+	exit
+fi
+
+if ! cp ${1}.pk3 ${build_webdir}/; then
+	exit
+fi
+
+if ! cp ${1}.pk3 ${build_serverdir}/; then
 	exit
 fi
 
 # sometimes the binary copied can already be running
-cp build/debug-linux-x86/tremded.x86 ~/.tremulousoc/oc/
+cp build/debug-linux-x86/tremded.x86 ${build_serverdir}/
