98f1fa6e342db3c2e6650b8f1ee0dcdb198dd5ca
bob <KrabbyKrap@gmail.com>
2009-08-23 19:17:16 -0600

    Fix 2D speedometer

--- src/cgame/cg_view.c
+++ src/cgame/cg_view.c
@@ -1498,7 +1498,7 @@ void CG_DrawActiveFrame( int serverTime, stereoFrame_t stereoView, qboolean demo
   if( cg_speedometerXYZ.integer )
     CG_AddSpeed( VectorLength( cg.snap->ps.velocity ) );
   else
-    CG_AddSpeed( cg.xyspeed );
+    CG_AddSpeed( sqrt( cg.snap->ps.velocity[ 0 ] * cg.snap->ps.velocity[ 0 ] + cg.snap->ps.velocity[ 1 ] * cg.snap->ps.velocity[ 1 ] ) );
 
   // build cg.refdef
   inwater = CG_CalcViewValues( );
