af08e6829519785ae1b919004eba90a34d2388de
bob <KrabbyKrap@gmail.com>
2009-10-24 08:18:22 -0600

    Fix typo in G_OC_NoTurretDroop

--- src/game/bg_oc.h
+++ src/game/bg_oc.h
@@ -651,7 +651,7 @@ extern int oc_gameMode;
 		} \
 	} while(0)
 
-	#define G_CG_OC_NoTurretDroop() ((BG_OC_OCMode()) ? (1) : (0))
+	#define G_OC_NoTurretDroop() ((BG_OC_OCMode()) ? (1) : (0))
 
 	#define G_OC_SelectAlienSpawnPoint() G_OC_SelectHumanSpawnPoint()
 
