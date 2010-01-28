ee6554d46e94ad01f52a9e7f2b3b7e0b3f459d33
bob <KrabbyKrap@gmail.com>
2010-01-28 21:07:56 +0000

    Increase both marauders' air acceleration from 2 to 3 before GPP balance gets updated

--- src/game/bg_misc.c
+++ src/game/bg_misc.c
@@ -1254,7 +1254,7 @@ static const classAttributes_t bg_classList[ ] =
     80,                                             //int     steptime;
     LEVEL2_SPEED,                                   //float   speed;
     10.0f,                                          //float   acceleration;
-    2.0f,                                           //float   airAcceleration;
+    3.0f,                                           //float   airAcceleration;
     6.0f,                                           //float   friction;
     100.0f,                                         //float   stopSpeed;
     380.0f,                                         //float   jumpMagnitude;
@@ -1281,7 +1281,7 @@ static const classAttributes_t bg_classList[ ] =
     80,                                             //int     steptime;
     LEVEL2_UPG_SPEED,                               //float   speed;
     10.0f,                                          //float   acceleration;
-    2.0f,                                           //float   airAcceleration;
+    3.0f,                                           //float   airAcceleration;
     6.0f,                                           //float   friction;
     100.0f,                                         //float   stopSpeed;
     380.0f,                                         //float   jumpMagnitude;
