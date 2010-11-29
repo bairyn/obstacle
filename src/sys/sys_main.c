fe58e7741c91f61ed886e6f2ad24370f3870caf2
Byron Johnson <KrabbyKrap@gmail.com>
2010-11-29 18:33:03 +0000

    Don't overwrite locale cvars to default values on every init

--- src/sys/sys_main.c
+++ src/sys/sys_main.c
@@ -196,8 +196,6 @@ void Sys_Init(void)
 	Cmd_AddCommand( "in_restart", Sys_In_Restart_f );
 	Cvar_Set( "arch", OS_STRING " " ARCH_STRING );
 	Cvar_Set( "username", Sys_GetCurrentUser( ) );
-	Cvar_Set( "locale", "/locale" );
-	Cvar_Set( "localepath", "" );
 	Sys_InitGettext( );
 }
 
